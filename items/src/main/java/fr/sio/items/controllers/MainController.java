1cfba3da3fd0bc95ad12b497a1c841de77dced78
J-C HERON <myaddressmail@gmail.com>
2022-08-31 17:01:40 +0200

    add form element

--- items/src/main/java/fr/sio/items/controllers/MainController.java
+++ items/src/main/java/fr/sio/items/controllers/MainController.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import fr.sio.items.models.Element;
 
@@ -27,8 +30,24 @@ public class MainController {
 	}
 
 	@GetMapping("add/{name}")
-	public @ResponseBody String addItem(@PathVariable String name, @SessionAttribute List<Element> items) {
+	public RedirectView addItem(@PathVariable String name, @SessionAttribute List<Element> items,
+			RedirectAttributes attrs) {
 		items.add(new Element(name));
-		return "Elément ajouté";
+		attrs.addFlashAttribute("msg", "Elément " + name + " ajouté");
+		return new RedirectView("/");
+	}
+
+	@GetMapping("add")
+	public RedirectView addForm(RedirectAttributes attrs) {
+		attrs.addFlashAttribute("form", true);
+		return new RedirectView("/");
+	}
+
+	@PostMapping("add")
+	public RedirectView postForm(@RequestParam String name, @SessionAttribute List<Element> items,
+			RedirectAttributes attrs) {
+		items.add(new Element(name));
+		attrs.addFlashAttribute("msg", "Elément " + name + " ajouté");
+		return new RedirectView("/");
 	}
 }
